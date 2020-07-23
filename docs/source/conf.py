fcd47436009936fe3ba3e45ba0e2e1832f501d11
Eshaan Bansal <eshaan7bansal@gmail.com>
2020-07-23 21:51:51 +0530

    bump to v1.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ copyright = "2020, Eshaan Bansal"
 author = "Eshaan Bansal"
 
 # The full version, including alpha/beta/rc tags
-release = "1.3.0"
+release = "1.4.0"
 
 
 # -- General configuration ---------------------------------------------------
