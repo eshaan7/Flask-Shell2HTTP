8fe12d75b2a06db67f30ced8df2280508d18f2fc
Eshaan Bansal <eshaan7bansal@gmail.com>
2020-10-06 17:41:46 +0530

    bump version number to v1.5.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ copyright = "2020, Eshaan Bansal"
 author = "Eshaan Bansal"
 
 # The full version, including alpha/beta/rc tags
-release = "1.5.1"
+release = "1.5.2"
 
 
 # -- General configuration ---------------------------------------------------
